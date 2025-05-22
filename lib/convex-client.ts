6111809e93de6644591f05ab4ad661f56098e1e9
Timofey Maximov <moltenship7750@gmail.com>
2025-05-22 20:48:41 +0400

    Add convex

---
+++ lib/convex-client.ts
@@ -0,0 +1,10 @@
+import { ConvexReactClient } from "convex/react";
+
+const CONVEX_URL = process.env.EXPO_PUBLIC_CONVEX_URL;
+
+if (!CONVEX_URL) {
+  throw new Error("Missing Convex URL");
+}
+
+export const convex = new ConvexReactClient(CONVEX_URL);
+

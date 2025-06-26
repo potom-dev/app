dbd531789b9fcd91faf441243c2e163be39567ca
moetazaneta <teqqmu@gmail.com>
2025-06-26 16:54:50 +0600

    add logo

---
+++ components/ui/Logo.tsx
@@ -0,0 +1,9 @@
+import { Heading } from "@/components/ui/heading";
+
+export function Logo({ className }: { className?: string }) {
+  return (
+    <Heading className={className}>
+      potom
+    </Heading>
+  )
+}
